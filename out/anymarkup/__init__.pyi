958cdc81de72d9debea14d895c4e2ecc38654ce5
Justine Kizhakkinedath <justine@kizhak.com>
2020-06-15 23:46:42 +0530

    fix: Mypy

---
+++ out/anymarkup/__init__.pyi
@@ -0,0 +1,1 @@
+from anymarkup_core import AnyMarkupError as AnyMarkupError, parse as parse, parse_file as parse_file, serialize as serialize, serialize_file as serialize_file
--- out/cerberus/platform.pyi
+++
@@ -1,4 +0,0 @@
-from collections.abc import Callable as Callable, Container as Container, Hashable as Hashable, Iterable as Iterable, Mapping as Mapping, MutableMapping as MutableMapping, Sequence as Sequence, Set as Set, Sized as Sized
-from typing import Any
-
-PYTHON_VERSION: Any
